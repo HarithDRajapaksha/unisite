b55531e4541ae01b5235e95e5dd6e70a17f58eea
HarithDRajapaksha <harithdevinda8@gmail.com>
2024-08-20 17:34:08 +0530

    Uni Project 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+//import react, hooks, and components
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// Import route pages
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Admin from './pages/Admin';
+import VehiclesDetails from './pages/VehiclesDetails';
+
+import { UserProvider } from './pages/UserContext';
+import EditUser from './pages/EditUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/Dashboard' element={<Dashboard/>}></Route>
+            <Route path='/Login' element={<Login/>}></Route>
+            <Route path='/Register' element={<Register/>}></Route>
+            <Route path='/Admin' element={<Admin/>}></Route>
+            <Route path='/Vehicles' element={<VehiclesDetails/>}></Route>
+            <Route path="/edit-user/:username" element={<EditUser/>} />
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
     </div>
   );
 }
